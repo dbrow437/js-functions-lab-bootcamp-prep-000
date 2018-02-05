8f3430249c60dc80b1d42d2f48e8ad7295c08bea
Damon Brown <dbrow437@gmail.com>
2018-02-05 19:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return 'Happyholidays, ${name}!';
+  return 'HappyHolidays, ${name}!';
 }
 
 function happyHolidayTo(holiday, name){
